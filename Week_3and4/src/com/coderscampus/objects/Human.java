a65ad36d1ae099d9bdb98170ec5ab38dab5b5806
fcynnek <kchengfung@gmail.com>
2022-10-14 14:55:49 -0400

    V 1.2

--- Week_3and4/src/com/coderscampus/objects/Human.java
+++ Week_3and4/src/com/coderscampus/objects/Human.java
@@ -14,11 +14,11 @@ public class Human {
 	}
 	
 	// defining inputs inside a method (String , String b, String c, String d, int e)
-	void setProperties (String _gender, String _height, String _weight, String _eyeColor) {
-		gender = _gender;
-		height = _height;
-		weight = _weight;
-		eyeColor = _eyeColor;
+	void setProperties (String gender, String height, String weight, String eyeColor) {
+		this.gender = gender;
+		this.height = height;
+		this.weight = weight;
+		this.eyeColor = eyeColor;
 		
 	}
 }
