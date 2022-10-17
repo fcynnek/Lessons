6ac5c5852eedd7a291907407a9071b2dfe4d7571
fcynnek <kchengfung@gmail.com>
2022-10-17 18:53:46 -0400

    Update MethodUserInputs.java

--- Week_3and4/src/com/fcynnek/week_3and4/MethodUserInputs.java
+++ Week_3and4/src/com/fcynnek/week_3and4/MethodUserInputs.java
@@ -1,5 +1,27 @@
 package com.fcynnek.week_3and4;
 
+import java.util.Scanner;
+
 public class MethodUserInputs {
 
+	public static void userInput() {
+		System.out.println("Type a number between 50 and 300: ");
+	
+	}
+	
+	public static void returnValues() {
+		int convertedInput = 0;
+		
+		if (convertedInput < 50) {
+			System.out.println("No");
+		}
+		
+		else if (convertedInput > 300) {
+			System.out.println("No!");
+		}
+		
+		else {
+			System.out.println("Yes!");
+		}
+	}
 }
