986c7b490278ee2ef3331b8303053d7de14db405
fcynnek <kchengfung@gmail.com>
2022-10-17 18:53:42 -0400

    Update Week3and4CreatingMethod.java

--- Week_3and4/src/com/fcynnek/week_3and4/Week3and4CreatingMethod.java
+++ Week_3and4/src/com/fcynnek/week_3and4/Week3and4CreatingMethod.java
@@ -6,25 +6,39 @@ public class Week3and4CreatingMethod {
 
 public static void main (String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Type a number between 50 and 300: ");
-		
-		String input = scanner.nextLine();
-		Integer convertedInput = Integer.parseInt(input);
-		
-		if (convertedInput < 50) {
-			System.out.println("No");
-		}
-		
-		else if (convertedInput > 300) {
-			System.out.println("No!");
-		}
-		
-		else {
-			System.out.println("Yes!");
-		}
-		
-		scanner.close();
+//		userInput();
+		validatingUserInput(userInput());
 		
 	}
+
+public static int userInput() {
+	
+	Scanner scanner = new Scanner (System.in);
+	System.out.println("Type a number between 50 and 300: ");
+	
+	String input = scanner.nextLine();
+	Integer convertedInput = Integer.parseInt(input);
+	
+	scanner.close();
+	return convertedInput;
+	
+}
+
+public static void validatingUserInput(int i) {
+	
+	int convertedInput = i;
+	
+	if (convertedInput < 50) {
+		System.out.println("The number you typed in was: " + null);
+	}
+	
+	else if (convertedInput > 300) {
+		System.out.println("The number you typed in was: " + null);
+	}
+	
+	else {
+		System.out.println("The number you typed in was: " + convertedInput);
+	}
+}
+
 }
