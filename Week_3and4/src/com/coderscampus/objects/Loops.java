9224a2104c4ab327d5949201186845d87d49394d
fcynnek <kchengfung@gmail.com>
2022-10-17 18:52:44 -0400

    V 1.0

---
+++ Week_3and4/src/com/coderscampus/objects/Loops.java
@@ -0,0 +1,14 @@
+package com.coderscampus.objects;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		// initialization ;termination condition; increment
+		for (int i = 1; i <= 10; i = i + 1) {
+			System.out.println(i);
+		}
+
+	}
+
+}
