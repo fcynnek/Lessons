599099166d4c9b2641dee696f2ef695897ea4bf2
fcynnek <kchengfung@gmail.com>
2022-10-17 20:48:00 -0400

    V 1.3

--- Week_3and4/src/com/coderscampus/objects/Loops.java
+++ Week_3and4/src/com/coderscampus/objects/Loops.java
@@ -5,6 +5,7 @@ public class Loops {
 	public static void main(String[] args) {
 		
 		// initialization ;termination condition; increment
+		// i = i + 1 can be shortcut as i++
 		for (int i = 1; i <= 10; i = i + 1) {
 			System.out.println(i);
 		}
