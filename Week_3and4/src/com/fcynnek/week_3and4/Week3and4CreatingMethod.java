571444cc808794ae7613bd339e3728c98740d206
fcynnek <kchengfung@gmail.com>
2022-10-19 18:02:28 -0400

    V final

--- Week_3and4/src/com/fcynnek/week_3and4/Week3and4CreatingMethod.java
+++ Week_3and4/src/com/fcynnek/week_3and4/Week3and4CreatingMethod.java
@@ -5,54 +5,59 @@ import java.util.Scanner;
 public class Week3and4CreatingMethod {
 
 public static void main (String[] args) {
-		
-//		userInput();
-		validatingUserInput(userInput());
-		
-	}
 
-public static int userInput() {
+	System.out.println("Type a number between 50 and 300: ");
 	
 	Scanner scanner = new Scanner (System.in);
-	System.out.println("Type a number between 50 and 300: ");
+//		userInput();
+	int lastNumber = userInput(scanner);
+		boolean callingUserInput = validatingUserInput(lastNumber);
+		
+		
+		while (callingUserInput == false) {
+			System.out.println("Oops, that number wasn't between 50 and 300, try again: ");
+			lastNumber = userInput(scanner);
+			callingUserInput = validatingUserInput(lastNumber);	
+		}
+		System.out.println("The number you typed in was: " + lastNumber);
+		
+		scanner.close();
+	}
+
+
+public static int userInput(Scanner scanner) {
+	
+
+
 	
 	String input = scanner.nextLine();
 	Integer convertedInput = Integer.parseInt(input);
 	
-	scanner.close();
+	
 	return convertedInput;
 	
 }
 
-public static void validatingUserInput(int i) {
+public static boolean validatingUserInput(int i) {
 	
 	int convertedInput = i;
-
 	
-//	if (convertedInput < 50) {
+	if (convertedInput < 50) {
 //		System.out.println("The number you typed in was: " + null);
-//	}
-//	
-//	else if (convertedInput > 300) {
+		return false;
+	}
+	
+	else if (convertedInput > 300) {
 //		System.out.println("The number you typed in was: " + null);
-//	}
+		return false;
+	}
 //	
 //	else {
 //		System.out.println("The number you typed in was: " + convertedInput);
 //	}
-	while (convertedInput !=i) {
-		
-		if (convertedInput != i) {
-			System.out.println("Oops, that number wasn't between 50 and 300, try again: ");
-		}
-		
-		else {
-			System.out.println("The number you typed in was: " + i);
-		}
+	return true;
 	}
 		
-		
-	}
 }
 
 
