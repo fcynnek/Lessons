2c52628a61d20a78e7745a214c6ea375e1a88c05
fcynnek <kchengfung@gmail.com>
2022-10-28 09:10:05 -0400

    V 0

---
+++ Week_5/src/com/coderscampus/week5/Human.java
@@ -0,0 +1,28 @@
+package com.coderscampus.week5;
+
+public class Human {
+	
+	// class variables
+	static int TEENAGER_AGE = 13;
+	static int ADULT_AGE = 19;
+	static int SENIOR_AGE = 65;
+	
+	// instance variable
+	int age;
+	
+	// All instantiations can access static variables but not the other way around
+	public String getStageOfLife () {
+		if (age < TEENAGER_AGE) {
+			return "Child";
+		}
+		else if (age < ADULT_AGE) {
+			return "Teenager";
+		}
+		else if (age < SENIOR_AGE) {
+			return "Adult";
+		}
+		else {
+			return "Senior";
+		}
+	}
+}
---
+++ Week_5/src/com/coderscampus/week5/StaticExample.java
@@ -0,0 +1,21 @@
+package com.coderscampus.week5;
+
+public class StaticExample {
+
+	// static = belongs to the class (StaticExample is the class), not an instance of the class
+	// the class can go with the method or the class (main is a method here)
+	// non-static (omit the word static) = belongs to the instance of the class (not the class itself)
+	public static void main(String[] args) {
+		StaticExample staticObject = new StaticExample ();
+		
+		if (staticObject.someNumber > 11) {
+			System.out.println("Say something");
+		}
+		
+		
+		Human aChild = new Human ();
+		aChild.age = 12;
+		System.out.println(aChild.getStageOfLife());
+	}
+
+}
