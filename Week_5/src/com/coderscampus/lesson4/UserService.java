da3768fbd761ff2a156b1df338e552e6a06a9360
fcynnek <kchengfung@gmail.com>
2022-11-02 17:35:12 -0400

    User Service

---
+++ Week_5/src/com/coderscampus/lesson4/UserService.java
@@ -0,0 +1,12 @@
+package com.coderscampus.lesson4;
+
+import com.coderscampus.lesson3.User;
+
+public class UserService {
+
+	public User createUser (String username, String password) {
+		// creating a new object
+		User user = new User();
+		return user;
+	}
+}
---
+++ Week_5/src/com/fcynnek/week5/CreatingUserPOJO.java
@@ -0,0 +1,28 @@
+package com.fcynnek.week5;
+
+public class CreatingUserPOJO {
+
+	public static void main(String[] args) {
+		
+		private String username;
+		private String password;
+		private String firstName;
+		private String lastName;
+
+	}
+	public void setUsername (String username) {
+		this.setUsername(username);
+	}
+	
+	public void setPassword (String password) {
+		this.setPassword(password);
+	}
+	
+	public void setFirstName (String firstName) {
+		this.setFirstName(firstName);
+	}
+
+	public void setLastName (String lastName) {
+		this.setLastName(lastName);
+	}
+}
