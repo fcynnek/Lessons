6f85518c7fab50473fa6521c0df1051439d24649
fcynnek <kchengfung@gmail.com>
2022-11-02 20:20:51 -0400

    Try 2

--- Week_5/src/com/fcynnek/week5/UserService.java
+++ Week_5/src/com/fcynnek/week5/UserService.java
@@ -2,8 +2,10 @@ package com.fcynnek.week5;
 
 public class UserService {
 
-	private String createUser (String username, String password, String name) {
-		User createUser = new User();
+	private void createUser () {
+		CreateUser[] createUser = new CreateUser[];
+		
+		
 	}
 	
 }
