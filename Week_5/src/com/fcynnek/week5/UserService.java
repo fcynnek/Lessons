3da281c8a58ecad16a90c5486e00b9cd24d4448e
fcynnek <kchengfung@gmail.com>
2022-11-10 09:25:06 -0500

    Copying lesson from check in

--- Week_5/src/com/fcynnek/week5/UserService.java
+++ Week_5/src/com/fcynnek/week5/UserService.java
@@ -1,5 +1,9 @@
 package com.fcynnek.week5;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.coderscampus.game.service.KennyService;
+
 public class UserService {
 
 	public void createUser (String username, String password, String name) {
@@ -13,6 +17,18 @@ public class UserService {
 		}
 	}
 		
+//	Calling a method from another class:
+	
+	// creating a method to substract 2 numbers
+//		public int substractingTwoNumbers (int first, int second) {
+//			int c = second - first;
+//			
+//			
+//			return c;
 	
 	
+//	KennyService kennyService = new KennyService();
+//	
+//	int result = kennyService.substractingTwoNumbers(3, 2);
+//	assertEquals (result, -1);
 }
