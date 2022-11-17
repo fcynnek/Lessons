b778c6cd4a3fab1ae9e88301382736cea69cad4e
fcynnek <kchengfung@gmail.com>
2022-11-17 18:35:58 -0500

    Lesson 7

---
+++ Week_6/src/com/coderscampus/lesson7/WrapperExampleApplication.java
@@ -0,0 +1,21 @@
+package com.coderscampus.lesson7;
+
+public class WrapperExampleApplication {
+
+	public static void main(String[] args) {
+
+		/**
+		 * byte		->	Byte
+		 * short	->	Short
+		 * int		->	Integer
+		 * float	->	Float
+		 * double	->	Double
+		 * char		->	Character
+		 * boolean	-> 	Boolean
+		 * 
+		 **/
+		
+		int primitiveInt = 8;
+	}
+
+}
