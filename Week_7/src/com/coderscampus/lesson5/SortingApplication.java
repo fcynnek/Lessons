12500a2ff40923cd99e59250513caadafd4f0f51
fcynnek <kchengfung@gmail.com>
2022-11-25 15:30:38 -0500

    Comparator lesson

--- Week_7/src/com/coderscampus/lesson5/SortingApplication.java
+++ Week_7/src/com/coderscampus/lesson5/SortingApplication.java
@@ -1,6 +1,7 @@
 package com.coderscampus.lesson5;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortingApplication {
 
@@ -28,12 +29,13 @@ public class SortingApplication {
 		System.out.println("----");
 		System.out.println("sorting with comparable method");
 		
-		Person[] people = new Person[4];
+		Person[] people = new Person[5];
 		
 		people[0] = new Person("Josh Allen", 72);
 		people[1] = new Person("Trevor Page", 70);
 		people[2] = new Person("Elon Musk", 74);
 		people[3] = new Person("Jordan Bellfast", 71);
+		people[4] = new Person("Jane Doe", 70);
 		Arrays.sort(people);
 		
 		// runtime exception because it needs a comparable object to be sorted
@@ -47,5 +49,29 @@ public class SortingApplication {
 			System.out.println(person.getName());
 	}
 	
+		// if we cannot have access to the source code and modify the code to gain access to the comparable method,
+		// there is another way to sort using the comparaTOR 
+		
+		// in this case, people variable is an array of person objects
+		// assuming that the person class has no access to the compare method
+		// Comparator needs to pass in the data type (in this case it is Person)
+		
+		System.out.println("----");
+		Arrays.sort(people, new Comparator<Person>() {
+
+			@Override
+			public int compare(Person person1, Person person2) {
+			// compare (Person 01, Person 02) Person is the data type. o1 and o2 are variable names
+				return person1.getHeight().compareTo(person2.getHeight());
+			}
+		});
+		for (Person person : people) {
+			System.out.println(person.getName() + " is " + person.getHeight() + " inches tall.");
+		}
+		
+		// another way of doing it is to create a whole new class implementing the comparator
+		// ie. a class called PersonHeightComparator implements Comparator<Person>
+		// implement the methods which will include the @override method in the new class
+		// instead of new Comparator<Person>() in line 60, we can have new PersonHeightComparator()
 	}
 }
