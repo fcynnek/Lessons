3d128c683d65c7b0a9120741b19c530554fd6719
fcynnek <kchengfung@gmail.com>
2022-12-27 18:26:58 -0500

    Overriding HashCode and Equals

--- Week_9/src/com/coderscampus/week9/SetApplication.java
+++ Week_9/src/com/coderscampus/week9/SetApplication.java
@@ -8,6 +8,35 @@ public class SetApplication {
 
 	public static void main(String[] args) {
 		
+//		example1();
+		
+		Set<Person> people = new HashSet<>();
+		
+		people.add(new Person(111111111, "Trevor", "Page"));
+		people.add(new Person(111111111, "Trevor2", "Page2"));
+		// Java needs to know that these 2 people are the same and that the 2nd entry is a duplicate
+		people.add(new Person(111111112, "Trevor", "Page"));
+		// this will output because the ssn returned true for being unique 
+		
+		for (Person person : people) {
+			System.out.println(person);
+			// this will call the toString because it will override the output as declared
+		}
+		
+		Set<String> uniqueNames = new HashSet<>();
+		// HashSets do NOT maintain order
+		
+		uniqueNames.add("Trevor Page");
+		uniqueNames.add("Elon Musk");
+		uniqueNames.add("Jane Doe");
+		uniqueNames.add("Josh Someone");
+		
+		for (String uniqueName : uniqueNames) {
+			System.out.println(uniqueName);
+		}
+	}
+
+	public static void example1() {
 		Set<Integer> uniqueNumbers = new LinkedHashSet<>();
 		// LinkedHashSet maintains order
 		
@@ -21,18 +50,6 @@ public class SetApplication {
 		
 		for (Integer uniqueNumber : uniqueNumbers) {
 			System.out.println(uniqueNumber);
-		}
-		
-		Set<String> uniqueNames = new HashSet<>();
-		// HashSets do NOT maintain order
-		
-		uniqueNames.add("Trevor Page");
-		uniqueNames.add("Elon Musk");
-		uniqueNames.add("Jane Doe");
-		uniqueNames.add("Josh Someone");
-		
-		for (String uniqueName : uniqueNames) {
-			System.out.println(uniqueName);
 		}
 	}
 }
