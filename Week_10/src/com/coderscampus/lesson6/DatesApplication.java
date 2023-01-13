00386f38176c43d340e81ecdb8daebc4ed00106b
fcynnek <kchengfung@gmail.com>
2023-01-13 17:28:26 -0500

    Dates

--- Week_10/src/com/coderscampus/lesson6/DatesApplication.java
+++ Week_10/src/com/coderscampus/lesson6/DatesApplication.java
@@ -1,8 +1,12 @@
 package com.coderscampus.lesson6;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.TimeZone;
 
 public class DatesApplication {
@@ -10,7 +14,43 @@ public class DatesApplication {
 	public static void main(String[] args) {
 
 //		lesson6();
+//		lesson7();
 		
+//		Date date = new Date();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2020, 0, 1);
+		Date jan1 = cal.getTime();
+		System.out.println(jan1);
+		
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); // HH=24hr format hh=12hr format
+		String formattedDate = formatter.format(jan1);
+		System.out.println(formattedDate);
+		
+		DateFormat formatter2 = new SimpleDateFormat("yyyy-MMM-dd hh:mm:ss z");
+		formatter2.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+		String formattedDate2 = formatter2.format(jan1);
+		System.out.println(formattedDate2);
+		
+		String dateValue1 = "2020-01-05 5:00 PM";
+		String dateValue2 = "2019-01-01 10:00 AM";
+		
+		DateFormat formatter3 = new SimpleDateFormat("yyyy-MM-dd h:mm a");
+		try {
+			Date date1 = formatter3.parse(dateValue1);
+			Date date2 = formatter3.parse(dateValue2);
+			if (date1.before(date2) ) {
+				System.out.println("date1 is before date2");
+			} else {
+				System.out.println("date1 is after date2");
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static void lesson7() {
 		Date jan012020 = new Date(2020,1,1,0,0,0); // YYYY, MM, DD, HH, MM, SS
 		System.out.println(jan012020);
 		
