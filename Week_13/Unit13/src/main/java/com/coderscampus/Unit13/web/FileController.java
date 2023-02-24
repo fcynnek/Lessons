5f92809345ec67c08a4e4b32bedc2ced3b75481c
fcynnek <kchengfung@gmail.com>
2023-02-23 19:56:19 -0500

    Get and Post Mapping

--- Week_13/Unit13/src/main/java/com/coderscampus/Unit13/web/FileController.java
+++ Week_13/Unit13/src/main/java/com/coderscampus/Unit13/web/FileController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coderscampus.Unit13.service.FileService;
@@ -21,8 +22,23 @@ public class FileController {
 	@Autowired
 	private FileService fileService;
 	
-	@GetMapping("")
+//	@GetMapping("") //empty "" means listening on the root of the application -> localhost:8080
+//	public List<String> readLines() throws IOException {
+//		return fileService.readFile("text.txt");
+//	}
+	
+	@GetMapping("/read-lines")
 	public List<String> readLines() throws IOException {
 		return fileService.readFile("text.txt");
+	}
+	
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	@PostMapping("/hello-world")
+	public String helloWorldPost() {
+		return "Okay cool, you just posted some data!";
 	}
 }
