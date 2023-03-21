56cd4a16e66b42a3e72218510a9270b63bc49b8d
fcynnek <kchengfung@gmail.com>
2023-03-20 20:03:29 -0400

    Thymeleaf

--- Week_15/Unit15/src/main/java/com/coderscampus/Unit15/web/WelcomeController.java
+++ Week_15/Unit15/src/main/java/com/coderscampus/Unit15/web/WelcomeController.java
@@ -1,15 +1,21 @@
 package com.coderscampus.Unit15.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 
 	@GetMapping("") // empty = localhost:8080
-	public String getRootWebpage() {
+	public String getRootWebpage(ModelMap model) {
 		// HTML files go under: src/main/resources -> templates{filename}.html
 		// filename should match the return statement here
+		// IMPORTANT: New dependency: Thymeleaf
+		// ModelMap is basically a hashmap (keys and values)
+		
+		String firstname = "Kenny";
+		model.put("firstname", firstname);
 		return "welcome";
 	}
 }
