2a06fa35b706ce823feb381bf4281d38a254b766
fcynnek <kchengfung@gmail.com>
2023-04-13 19:52:05 -0400

    Adding an extra user

--- Week_16/3. Adding data to table.sql
+++ Week_16/3. Adding data to table.sql
@@ -12,3 +12,6 @@ values (3, 'jane@doe.com', 'password123', 'Jane', 'Doe');
 
 insert into users (user_id, username, password, first_name, last_name)
 values (4, 'elon@musk.com', 'asdfasdf', 'Elon', 'Musk');
+
+insert into users (user_id, username, password, first_name, last_name)
+values (5, 'jane@musk.com', 'asdfasdf', 'Jane', 'Musk');
