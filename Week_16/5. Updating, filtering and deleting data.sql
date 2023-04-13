7b28c0f6d6e841e9fce18b05d806deec0e0819ef
fcynnek <kchengfung@gmail.com>
2023-04-13 08:44:06 -0400

    Update, Delete and Deleting Data

---
+++ Week_16/5. Updating, filtering and deleting data.sql
@@ -0,0 +1,27 @@
+select * from transactions;
+
+update transactions
+set user_id = 1;
+
+-- selecting data using a primary key
+select * from transactions
+where transaction_id = 2;
+
+select * from users
+where first_name = 'Jane'
+and last_name = 'Musk';
+
+update users
+set last_name = 'Musk'
+where password = 'password123';
+
+update transactions
+set user_id = 2
+where transaction_id = 3;
+
+delete from users
+where user_id = 7;
+
+select * from users
+where first_name = 'Jane'
+or first_name = 'John';
