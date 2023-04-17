db3db5f0d589505176a8116ebf188fe98f79a275
fcynnek <kchengfung@gmail.com>
2023-04-17 18:27:18 -0400

    Ignoring Softwares

---
+++ Week_18/Unit18/src/main/java/com/coderscampus/Unit18/Unit18Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit18Application.class, args);
+	}
+
+}
---
+++ Week_18/Unit18/src/test/java/com/coderscampus/Unit18/Unit18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
