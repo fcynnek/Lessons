c300b6ece82d94b34076510581f8e15eede84e22
fcynnek <kchengfung@gmail.com>
2023-05-08 20:13:34 -0400

    Initial commit for Unit 20

---
+++ Week_20/Unit20/src/test/java/com/coderscampus/Unit20/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
