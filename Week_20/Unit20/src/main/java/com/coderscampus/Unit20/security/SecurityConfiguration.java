cc10ddd0a641d781d926a25c4a9e3c0d45e38ed2
fcynnek <kchengfung@gmail.com>
2023-05-10 20:06:56 -0400

    Password Encrypter

--- Week_20/Unit20/src/main/java/com/coderscampus/Unit20/security/SecurityConfiguration.java
+++ Week_20/Unit20/src/main/java/com/coderscampus/Unit20/security/SecurityConfiguration.java
@@ -1,12 +1,24 @@
 package com.coderscampus.Unit20.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -33,4 +45,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.defaultSuccessUrl("/dashboard")
 			.permitAll();
 	}
+	
 }
