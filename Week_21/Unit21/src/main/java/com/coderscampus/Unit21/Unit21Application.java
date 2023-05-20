aed65c2b814d535e3dfda39e75c60ba5bcff26c1
fcynnek <kchengfung@gmail.com>
2023-05-20 17:05:55 -0400

    Initializing Unit 21

---
+++ Week_21/Unit21/src/main/java/com/coderscampus/Unit21/Unit21Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit21Application.class, args);
+	}
+
+}
---
+++ Week_21/Unit21/src/test/java/com/coderscampus/Unit21/Unit21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
