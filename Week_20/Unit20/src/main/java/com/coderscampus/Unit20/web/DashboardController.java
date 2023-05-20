acb6e49aa09e049e320ab71ea5942bbc3aa39a46
fcynnek <kchengfung@gmail.com>
2023-05-20 16:58:42 -0400

    Spring Security

--- Week_20/Unit20/src/main/java/com/coderscampus/Unit20/web/DashboardController.java
+++ Week_20/Unit20/src/main/java/com/coderscampus/Unit20/web/DashboardController.java
@@ -1,19 +1,27 @@
 package com.coderscampus.Unit20.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.coderscampus.Unit20.domain.User;
+import com.coderscampus.Unit20.service.AdminService;
 
 @Controller
 public class DashboardController {
 
+	@Autowired
+	private AdminService adminService;
+	
 	@GetMapping("/dashboard")
 	public String getDashboard (@AuthenticationPrincipal User user, ModelMap model) {
 		System.out.println(user);
 		model.put("user", user);
+		List<User> allUserAccounts = adminService.getAllUserAccounts();
 		return "dashboard";
 	}
 }
