877736642abb9d52aa81b6a06544638410a69e1e
fcynnek <kchengfung@gmail.com>
2023-08-15 20:26:51 -0400

    Rewatch 24

--- Week_20Extra/Unit20Extra/src/main/java/com/coderscampus/security/Unit20Extra/security/JwtAuthenticationFilter.java
+++ Week_20Extra/Unit20Extra/src/main/java/com/coderscampus/security/Unit20Extra/security/JwtAuthenticationFilter.java
@@ -3,29 +3,43 @@ package com.coderscampus.security.Unit20Extra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.coderscampus.security.Unit20Extra.service.JwtService;
 import com.coderscampus.security.Unit20Extra.service.UserService;
 
+import jakarta.persistence.Entity;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	@Autowired
+//	@Autowired
 	private JwtService jwtService;
 	
-	@Autowired
+//	@Autowired
 	private UserService userService;
 	
+	
+	public JwtAuthenticationFilter(JwtService jwtService, UserService userService) {
+	super();
+	this.jwtService = jwtService;
+	this.userService = userService;
+}
+
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
@@ -37,12 +51,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		
 		String authHeader = request.getHeader("Authorization");
 		
-		if (!authHeader.isEmpty()) {
+		if (StringUtils.hasText(authHeader)) {
 			String token = authHeader.substring(7); //Bearer + space = 7
 			String subject = jwtService.getSubject(token);
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 			
-			if (!subject.isEmpty() && authentication == null) {
+			if (StringUtils.hasText(subject) && authentication == null) {
 				UserDetails userDetails = userService.loadUserByUsername(subject);
 				
 				if (jwtService.isValidToken(token, userDetails)) {
